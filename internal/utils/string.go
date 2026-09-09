37e08ea55d230803f8a370fe6e1671d742f93661
qingmei <qingmei@tencent.com>
2026-09-09 18:51:18 +0800

    feat: Add app subcommand with set/get and validators

--- internal/utils/string.go
+++ internal/utils/string.go
@@ -12,3 +12,19 @@ func CharacterLimit(flag, str string, limit int) error {
 	}
 	return nil
 }
+
+// CharacterWidthLimit character width limit: ASCII counts as 1, non-ASCII (e.g. Chinese) counts as 2.
+func CharacterWidthLimit(flag, str string, limit int) error {
+	width := 0
+	for _, r := range str {
+		if r < utf8.RuneSelf {
+			width++
+		} else {
+			width += 2
+		}
+	}
+	if width > limit {
+		return exception.InvalidArgsError.With("flag:%s, character width limit exceeded, limit: %d, actual: %d", flag, limit, width)
+	}
+	return nil
+}
